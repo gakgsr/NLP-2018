e71ddfb9942eab98195c6aed64715b12a90df540
gakgsr <gakgsr@gmail.com>
2018-03-08 19:54:20 -0800

    Added few more layers

--- code/bidafmodule.py
+++ code/bidafmodule.py
@@ -45,7 +45,7 @@ class Attention_layer4(object):
         # self.embed_dim = embed_dim
         self.keep_prob = keep_prob
 
-    def build_graph(self, H, U):
+    def build_graph(self, H, U, U_mask):
         with vs.variable_scope("Attention"):
             H=tf.transpose(H,perm=[0,2,1])
             U=tf.transpose(U,perm=[0,2,1])
@@ -63,7 +63,8 @@ class Attention_layer4(object):
             p2=tf.tensordot(w2, U, axes=[[0], [1]])   # shape (batch_size, question_len)
             p3_1=tf.multiply(tf.expand_dims(H,3),tf.expand_dims(U,2))  # shape (batch_size,embedding_size, contex_len, question_len)
             p3=tf.tensordot(w3, p3_1,axes=[[0],[1]]) # batch_dim contex_len question_len
-            S=tf.add(tf.add(tf.expand_dims(p1, 2), tf.expand_dims(p2, 1)), p3) # batch_dim contex_len question_len
+            S_hat = tf.add(tf.add(tf.expand_dims(p1, 2), tf.expand_dims(p2, 1)), p3) # batch_dim contex_len question_len
+            _, S = masked_softmax(S_hat, tf.expand_dims(U_mask, 1), 2)
             a=tf.nn.softmax(S,dim=1) # batch_dim contex_len question_len
             b=tf.nn.softmax(tf.reduce_max(S,reduction_indices=2),dim=1)  # batch_dim contex_len
             U_hat=tf.matmul(U,tf.transpose(a,perm=[0,2,1])) #batch_dim embed_dim context_len
@@ -84,8 +85,8 @@ class BiLSTM_layer5(object):
         self.rnn_cell_bw = tf.contrib.rnn.BasicLSTMCell(self.hidden_size, forget_bias=1.0)
         self.rnn_cell_bw = DropoutWrapper(self.rnn_cell_bw, input_keep_prob=self.keep_prob)
 
-    def build_graph(self, inputs):
-        with vs.variable_scope("BiLSTM5"):
+    def build_graph(self, inputs, scope_val):
+        with vs.variable_scope(scope_val):
             # input_lens = tf.reduce_sum(masks, reduction_indices=1) # shape (batch_size)
 
             # Note: fw_out and bw_out are the hidden states for every timestep.
@@ -101,17 +102,33 @@ class BiLSTM_layer5(object):
 
             return out
 class OutputLayer_6(object):
-    def __init__(self):
-        pass
+    def __init__(self, hidden_size, keep_prob):
+        self.hidden_size = hidden_size
+        self.keep_prob = keep_prob
+        self.rnn_cell_fw = tf.contrib.rnn.BasicLSTMCell(self.hidden_size, forget_bias=1.0)
+        self.rnn_cell_fw = DropoutWrapper(self.rnn_cell_fw, input_keep_prob=self.keep_prob)
+        self.rnn_cell_bw = tf.contrib.rnn.BasicLSTMCell(self.hidden_size, forget_bias=1.0)
+        self.rnn_cell_bw = DropoutWrapper(self.rnn_cell_bw, input_keep_prob=self.keep_prob)
 
     def build_graph(self, G, M):
         with vs.variable_scope("OutputLayer"):
 
             # Linear downprojection layer
+            (fw_out, bw_out), (out_fw_state,out_bw_state) = tf.nn.bidirectional_dynamic_rnn(self.rnn_cell_fw, self.rnn_cell_bw, tf.transpose(M, perm=[0, 2, 1]), dtype=tf.float32)
+            M2 = tf.transpose(tf.concat([fw_out, bw_out], 2), perm = [0, 2, 1])
+
             G_shap=G.get_shape().as_list()
             M_shap=M.get_shape().as_list()
+            M2_shap=M2.get_shape().as_list()
             w1=tf.get_variable("w1",[G_shap[1]+M_shap[1]], initializer=tf.contrib.layers.xavier_initializer())
-            w2=tf.get_variable("w2",[G_shap[1]+M_shap[1]], initializer=tf.contrib.layers.xavier_initializer())
+            w2=tf.get_variable("w2",[G_shap[1]+M2_shap[1]], initializer=tf.contrib.layers.xavier_initializer())
+
             p1=tf.nn.softmax(tf.tensordot(w1, tf.concat([G,M],1), axes=[[0], [1]]))
-            p2=tf.nn.softmax(tf.tensordot(w2, tf.concat([G,M],1), axes=[[0], [1]]))
+            p2=tf.nn.softmax(tf.tensordot(w2, tf.concat([G,M2],1), axes=[[0], [1]]))
             return p1,p2
+
+def masked_softmax(logits, mask, dim):
+    exp_mask = (1 - tf.cast(mask, 'float')) * (-1e30) # -large where there's padding, 0 elsewhere
+    masked_logits = tf.add(logits, exp_mask) # where there's padding, set logits to -large
+    prob_dist = tf.nn.softmax(masked_logits, dim)
+    return masked_logits, prob_dist
--- code/qa_model.py
+++ code/qa_model.py
@@ -154,12 +154,13 @@ class QAModel(object):
 
         # Use context hidden states to attend to question hidden states
         attn_layer = Attention_layer4(self.keep_prob)
-        attn_output = attn_layer.build_graph(context_hiddens, question_hiddens )
+        attn_output = attn_layer.build_graph(context_hiddens, question_hiddens, self.qn_mask)# (batch_size, context_len, hidden_size*8)
 
         encoder1 = BiLSTM_layer5(self.FLAGS.hidden_size, self.keep_prob)
-        encode_out = tf.transpose(encoder1.build_graph(tf.transpose(attn_output, perm=[0, 2, 1])), perm=[0, 2, 1])
+        encode_out_1 = tf.transpose(encoder1.build_graph(tf.transpose(attn_output, perm=[0, 2, 1])), perm=[0, 2, 1], "BiLSTM5_1")# (batch_size, context_len, hidden_size*2)
+        encode_out = tf.transpose(encoder1.build_graph(tf.transpose(encode_out_1, perm=[0, 2, 1])), perm=[0, 2, 1], "BiLSTM5_2")# (batch_size, context_len, hidden_size*2)
 
-        softm = OutputLayer_6()
+        softm = OutputLayer_6(self.FLAGS.hidden_size, self.keep_prob)
         self.probdist_start,self.probdist_end = softm.build_graph(attn_output,encode_out)
 
         # Concat attn_output to context_hiddens to get blended_reps
